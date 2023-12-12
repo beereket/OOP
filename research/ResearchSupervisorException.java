e69cd6e7c59fba31129095fddad08f92711af681
Daniyal Tuzelbayev <91614969+holydanchik@users.noreply.github.com>
2023-12-12 19:47:10 +0600

    Add files via upload

---
+++ research/ResearchSupervisorException.java
@@ -0,0 +1,12 @@
+package research;
+
+public class ResearchSupervisorException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResearchSupervisorException(String message) {
+        super(message);
+    }
+}
