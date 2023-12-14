89a4876ecbd7b9ba8705b5e9b3df68b3e5589356
holydanchik <tuzelbaeff@gmail.com>
2023-12-14 19:04:01 +0600

    Research - created directories

---
+++ Research/Exceptions/ResearchProjectJoinException.java
@@ -0,0 +1,12 @@
+package Research;
+
+public class ResearchProjectJoinException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResearchProjectJoinException(String message) {
+        super(message);
+    }
+}
